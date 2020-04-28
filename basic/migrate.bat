45ae99a44f0e161c653880afa0920ae5806d158d
Ondriy <reactyii@mail.ru>
2020-04-28 08:43:41 +0300

    script for apply migrations

---
+++ basic/migrate.bat
@@ -0,0 +1,13 @@
+@rem https://www.yiiframework.com/doc/guide/2.0/ru/db-migrations#reverting-migrations
+
+@yii migrate --interactive=0
+
+@rem повторить последние 1
+@rem yii migrate/redo 1
+
+@rem отменить последние 1
+@rem yii migrate/down 1
+
+@rem документация по миграциям
+@rem https://www.yiiframework.com/doc/guide/2.0/ru/db-migrations
+@rem https://www.yiiframework.com/doc/api/2.0/yii-db-schemabuildertrait
---
+++ basic/migrate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#https://www.yiiframework.com/doc/guide/2.0/ru/db-migrations#reverting-migrations
+
+yii migrate --interactive=0
+
+#повторить последние 1
+#yii migrate/redo 1
+
+#отменить последние 1
+#yii migrate/down 1
+
+#документация по миграциям
+#https://www.yiiframework.com/doc/guide/2.0/ru/db-migrations
+#https://www.yiiframework.com/doc/api/2.0/yii-db-schemabuildertrait
